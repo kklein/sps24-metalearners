Fix computation of policy value.

src/script.py
@@ -275,19 +275,17 @@ def step_6(
         )
     )
     budget_indices = cate_estimates_rlearner.argsort()[-budget:][::-1]
-    policy = np.zeros(df.shape[0], dtype=int)
-    policy[budget_indices] = 1
+    cate_policy = np.zeros(df.shape[0], dtype=int)
+    cate_policy[budget_indices] = 1
     # only if they are positive
-    policy = policy * (cate_estimates_rlearner > 0)
+    cate_policy = cate_policy * (cate_estimates_rlearner > 0)
 
-    policy_value = _policy_value(
-        policy=policy,
+    policy_value_cate = _policy_value(
+        policy=cate_policy,
         treatment=df[treatment_column],
         outcome=df[outcome_column],
         propensity_scores=propensity_scores,
     )
-
-    print(f"Policy value of treating the top {budget} students: {policy_value}")
 
     policy_value_0 = _policy_value(
         np.zeros(df.shape[0], dtype=int),
@@ -303,8 +301,21 @@ def step_6(
         propensity_scores=propensity_scores,
     )
 
-    policy_value_uar = (policy_value_1 - policy_value_0) * budget / len(df)
+    policy_value_uar = (policy_value_1 - policy_value_0) * budget / len(df) + policy_value_0
 
+    experiment_policy = df[treatment_column]
+    experiment_policy_value = _policy_value(
+        experiment_policy,
+        treatment=df[treatment_column],
+        outcome=df[outcome_column],
+        propensity_scores=propensity_scores,
+    )
+
+    print(f"{policy_value_0=}")
+    print(f"{policy_value_1=}")
+    print(f"{experiment_policy_value=}")
+
+    print(f"Policy value of treating the top {budget} students: {policy_value_cate}")
     print(f"Policy value of treating {budget} students u.a.r.: {policy_value_uar}")
 
 
@@ -361,12 +372,12 @@ def main():
 
     rlearner = step_3(df, outcome_column, treatment_column, feature_columns)
 
-    step_4(df, feature_columns, outcome_column, treatment_column)
+    rlearner_tuned = step_4(df, feature_columns, outcome_column, treatment_column)
 
-    step_5(rlearner, df, feature_columns)
+    step_5(rlearner_tuned, df, feature_columns)
 
     step_6(
-        rlearner,
+        rlearner_tuned,
         df,
         feature_columns,
         treatment_column,